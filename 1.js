dceb577ee55d50afe079197cfd03856b7e0d174d
AlexeySkrylnikov <77483136+AlexeySkrylnikov@users.noreply.github.com>
2022-05-21 23:47:03 +0500

    ДЗ по 1 уроку готово

--- 1.js
+++ 1.js
@@ -5,3 +5,9 @@ Tf = (9 / 5) * Tc + 32;
 где Tf – температура по Фаренгейту, Tc – температура по Цельсию
 */
 
+"use strict";
+let tempInCelsius = null;
+let tempInFahrenheit = null;
+tempInCelsius = +prompt('Укажите температуру в Цельсиях');
+tempInFahrenheit = (9 / 5) * tempInCelsius + 32;
+alert(`Темепература в Фаренгейтах: ${tempInFahrenheit}`);
